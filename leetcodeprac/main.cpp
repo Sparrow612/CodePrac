b47395c99a9c8f5c37ca21da259f7b348d882e6c
程荣鑫 <rongxinCRX@outlook.com>
2020-04-04 22:07:30 +0800

    Initial Commit

---
+++ leetcodeprac/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  leetcodeprac
+//
+//  Created by 程荣鑫 on 2020/4/4.
+//  Copyright © 2020 程荣鑫. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
