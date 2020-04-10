e1a1ab2c8c3a527d963d83d90e963daae06b5e25
程荣鑫 <rongxinCRX@outlook.com>
2020-04-10 22:14:57 +0800

    final update on 2020.04.10

--- leetcodeprac/shortestPalindrome.cpp
+++ leetcodeprac/shortestPalindrome.cpp
@@ -11,20 +11,37 @@
 
 using namespace std;
 
+//string shortestPalindrome(string s) {
+//    int n=(int)s.size();
+//    string rev(s);
+//    reverse(rev.begin(), rev.end());
+//    for (int i=0;i<n;i++){
+//        if (s.substr(0,n-i)==rev.substr(i))
+//            return rev.substr(0,i)+s;//找到最长的回文子串
+//    }
+//    return "";
+//}
+
+
+//上面是最简单直白的暴力求解，事实证明过不了
+
 string shortestPalindrome(string s) {
     int n=(int)s.size();
-    string rev(s);
-    reverse(rev.begin(), rev.end());
-    for (int i=0;i<n;i++){
-        if (s.substr(0,n-i)==rev.substr(i))
-            return rev.substr(0,i)+s;//找到最长的回文子串
+    int i=0;
+    for (int j=n-1;j>=0;j--){
+        if (s[i]==s[j])
+            i++;
     }
-    return "";
+    if (i==n) return s;
+    string tmp=s.substr(i);
+    reverse(tmp.begin(), tmp.end());
+    return tmp+shortestPalindrome(s.substr(0,i))+s.substr(i);
 }
+
 
 int main(){
     string src="aacecaaa";
     cout<<shortestPalindrome(src)<<endl;
 }
 
-//最简单直白的暴力求解
+//这里的算法是双指针+递归
